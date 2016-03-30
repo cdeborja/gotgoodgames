6e4c04a4f58201bb5fc0422436a7fcfbf5aa5fa3
cdeborja <cdeborja@gmail.com>
2016-03-30 08:40:25 -0400

    Add valid Game seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.destroy_all
+
+Game.destroy_all
+game1 = Game.create!(title: "Mario is missing",
+        description: "You have to find mario",
+        release_date: "29-01-1992")
+game2 = Game.create(title: "Legend of Zelda",
+        description: "A young boy journeys off to find Zelda",
+        release_date: "21-02-1986")
+game3 = Game.create!(title: "Notice me Senpai",
+        description: "You have to talk to your Senpai... for forever",
+        release_date: "01-01-2015")
+game4 = Game.create!(title: "Tetris",
+        description: "Match blocks, get points",
+        release_date: "06-06-1984")
