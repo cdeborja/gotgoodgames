9aa88657151c6a72f53699f14a2416aa2a105e22
cdeborja <cdeborja@gmail.com>
2016-05-25 12:49:48 -0400

    Fix loading screen

--- frontend/components/users/usersIndex.jsx
+++ frontend/components/users/usersIndex.jsx
@@ -40,7 +40,7 @@ module.exports = React.createClass({
 
   render: function () {
     if (this.state.users.length === 0) {
-      return (<div className="loading"> Loading... </div>);
+      return (<img className="loading-image" src="https://www.criminalwatchdog.com/images/assets/loading.gif"/>);
     }
     var that = this;
     var usersIndex = this.state.users.map(function (user) {
